1956af32d49b944af0eddf9e068f85a6dcd372f6
FernandoTejedorL <fernandotejedorlopez@gmail.com>
2025-01-09 19:34:42 +0100

    completed

--- src/pages/user/User.jsx
+++
@@ -1,8 +0,0 @@
-import { useParams } from 'react-router-dom';
-
-const User = () => {
-	//const params = useParams();
-	const { username } = useParams();
-	return <h1>{username}</h1>;
-};
-export default User;
